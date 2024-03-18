384947874c42500be449923fa6ba8f5dd14e36f6
pinae <pina@pinae.net>
2024-03-18 12:38:12 +0100

    Two lines

--- train-MNIST.py
+++ train-MNIST.py
@@ -77,7 +77,8 @@ def train(dataloader, model, loss_fn, optimizer):
         optimizer.zero_grad()
 
         if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(X)
+            loss = loss.item()
+            current = (batch + 1) * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
